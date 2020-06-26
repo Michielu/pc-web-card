b1ab376701e153f6f34d535e351a3a06c0ee93c2
Michielu Menning <mich.menning@gmail.com>
2020-06-25 20:19:21 -0500

    Add card services

--- card-microservice/server/routes/cards.js
+++ card-microservice/server/routes/cards.js
@@ -3,27 +3,35 @@ import cardService from '../services/cardServices';
 
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/all', function (req, res, next) {
+router.get('/all', function (req, res) {
   res.send(cardService.getAllCards());
 });
 
-router.get('/bank/:bankId', function (req, res, next) {
+router.get('/bank/:bankId', function (req, res) {
   res.send(cardService.getAllCardsFromBank(req.params));
 });
 
-router.get('/categ/all', function (req, res, next) {
-  res.send(cardService.getAllCardsFromBank(req.params));
+router.get('/categ/all', function (req, res) {
+  res.send(cardService.getAllCategories6(req.params));
 });
 
-router.get('/categ/staple', function (req, res, next) {
+router.get('/categ/staple', function (req, res) {
   res.send(cardService.getCertainCategory());
 });
 
-router.get('/categ/special', function (req, res, next) {
+router.get('/categ/special', function (req, res) {
   res.send(cardService.getCertainCategory(false));
 });
 
+//Get rewards for card
+router.post('/rewards', (req, res) => {
+  res.send(cardService.getCardReward(req.body.cardId));
+})
 
+//Best card for category
+//Add own card (that's not in my db)
+router.post('/rewards/best', (req, res) => {
+  res.send(cardService.getBestRewardCard(req.body));
+})
 
 export default router;
--- card-microservice/server/services/cardServices.js
+++ card-microservice/server/services/cardServices.js
@@ -1,12 +1,13 @@
 import banks from '../../data/banks.json';
 import categories from '../../data/categories.json'
+import cards from '../../data/cards.json'
+
 
 const getAllCards = () => {
     return banks;
 }
 
 const getAllCardsFromBank = ({ bankId }) => {
-    console.log("bankId: ", bankId)
     return banks[bankId];
 }
 
@@ -18,9 +19,28 @@ const getCertainCategory = (staple = true) => {
     return staple ? categories["staples"] : categories["specialties"];
 }
 
+const getCardReward = (cardId) => {
+    return cards[cardId];
+}
+
+const getBestRewardCard = ({ cardList, category, parentCategory }) => {
+    const cardWithValue = cardList.map(c => {
+        console.log("getCard", c, getCardReward(c)[parentCategory][category])
+        return {
+            [c]: getCardReward(c)[parentCategory][category]
+        }
+    });
+
+    return cardWithValue.sort((a, b) => {
+        return Object.values(b)[0] - a[Object.keys(a)[0]];
+    });
+}
+
 export default Object.freeze({
     getAllCards,
     getAllCardsFromBank,
     getAllCategories,
-    getCertainCategory
+    getCertainCategory,
+    getCardReward,
+    getBestRewardCard
 })
