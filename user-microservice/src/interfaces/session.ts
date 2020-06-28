a0a086e64f6c969bf7bb272b0f109e207fe39895
Michielu Menning <mich.menning@gmail.com>
2020-06-28 09:27:52 -0500

    Create session service

---
+++ user-microservice/src/interfaces/session.ts
@@ -0,0 +1,6 @@
+export class LocalSession {
+    userId: string;
+    username: string;
+    isLoggedIn: boolean;
+    cards: string[];
+}
---
+++ user-microservice/src/providers/session.provider.ts
@@ -0,0 +1,52 @@
+//Manage session
+import { Injectable } from '@nestjs/common';
+import { LocalSession } from '../interfaces/session';
+
+@Injectable()
+export class SessionProvider {
+    private userSession: LocalSession = null;
+
+    setUserId(userId: string) {
+        this.userSession.userId = userId;
+    }
+
+    getUserId(): string {
+        return this.userSession.userId;
+    }
+
+    setUsername(username: string) {
+        this.userSession.username = username;
+    }
+
+    getUsername(): string {
+        return this.userSession.username;
+    }
+
+    setLogin(loggedIn: boolean) {
+        this.userSession.isLoggedIn = loggedIn;
+    }
+
+    getLoggedIn(): boolean {
+        return this.userSession.isLoggedIn;
+    }
+
+    setCards(cards: string[]) {
+        this.userSession.cards = cards;
+    }
+
+    addCards(card: string) {
+        this.userSession.cards.push(card);
+    }
+
+    removeCard(card: string) {
+        this.userSession.cards = this.userSession.cards.filter(c => { c !== card })
+    }
+
+    getCards(): string[] {
+        return this.userSession.cards;
+    }
+
+    clear() {
+        this.userSession = null;
+    }
+}
